bc9cdca578c072ca04d8cf0cad60367a1dbfd439
Brian Lee <brian.lee@ge.com>
2017-10-20 14:09:56 -0700

    Fixed AppContainer component for App relative path

--- packages/react-scripts/template/src/containers/AppContainer/AppContainer.js
+++ packages/react-scripts/template/src/containers/AppContainer/AppContainer.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import App from 'components/App';
+import App from '../../components/App';
 import helloActions from 'actions/hello';
 
 const mapStateToProps = state => ({
