7849f6863690b66b43c4d9020a152034c7872d9d
josh sylvester <josh.sylvester@ge.com>
2018-03-14 14:54:34 -0700

    UI-1066 adding dynamic paths to library.

--- packages/react-scripts/config/paths.js
+++ packages/react-scripts/config/paths.js
@@ -98,12 +98,19 @@ if (
   !reactScriptsLinked &&
   __dirname.indexOf(path.join('packages', 'react-scripts', 'config')) !== -1
 ) {
+  let indexHTMLFile = 'template/public/index.html';
+  if (fs.existsSync(resolveOwn('node_modules/@svmx/ui-components-lightning')) ) {
+    indexHTMLFile = 'template/public/index-lightning.html';
+  } else if (fs.existsSync(resolveOwn('node_modules/@svmx/ui-components-predix')) ) {
+    indexHTMLFile = 'template/public/index-predix.html';
+  }
+
   module.exports = {
     dotenv: resolveOwn('template/.env'),
     appPath: resolveApp('.'),
     appBuild: resolveOwn('../../build'),
     appPublic: resolveOwn('template/public'),
-    appHtml: resolveOwn('template/public/index.html'),
+    appHtml: resolveOwn(indexHTMLFile),
     appIndexJs: resolveOwn('template/src/index.js'),
     appPackageJson: resolveOwn('package.json'),
     appSrc: resolveOwn('template/src'),
---
+++ packages/react-scripts/scripts/salesforce.js
@@ -0,0 +1,36 @@
+// The following environment vars are required when running a salesforce build.
+//   REACT_APP_SF_BUILD_FOLDER = ''
+//   REACT_APP_SF_STATIC_RESOURCE = ''
+const spawn = require('react-dev-utils/crossSpawn');
+
+process.env.NODE_ENV = 'production';
+require('../config/env');
+
+const buildFolder = process.env.REACT_APP_SF_BUILD_FOLDER || 'CreateReactApp';
+const staticResource = process.env.REACT_APP_SF_STATIC_RESOURCE || 'create-react-app';
+const newPublicUrl = `{!URLFOR($Resource.${staticResource}, '${buildFolder}')}/`;
+
+const env = Object.create(process.env);
+env.PUBLIC_URL = newPublicUrl;
+
+const result = spawn.sync('node', [`${__dirname}/build.js`], { env });
+
+if (result.signal) {
+  if (result.signal === 'SIGKILL') {
+    console.log(
+      'The build failed because the process exited too early. ' +
+        'This probably means the system ran out of memory or someone called ' +
+        '`kill -9` on the process.',
+    );
+  } else if (result.signal === 'SIGTERM') {
+    console.log(
+      'The build failed because the process exited too early. ' +
+        'Someone might have called `kill` or `killall`, or the system could ' +
+        'be shutting down.',
+    );
+  }
+  process.exit(1);
+} else {
+  console.log('completed saleforce build');
+}
+process.exit(result.status);
