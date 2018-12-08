c47d25eb4da2aba2f94d87ac9a13b3c3c41a0ce5
josh sylvester <josh.sylvester@ge.com>
2018-12-07 17:44:12 -0800

    UI-3007 changing the location to look for salesforce assets back to the original location

--- packages/react-scripts/config/webpack.config.dev.js
+++ packages/react-scripts/config/webpack.config.dev.js
@@ -38,7 +38,6 @@ const env = getClientEnvironment(publicUrl);
 // Get possible module paths
 const appNodeModules = paths.appNodeModules;
 const uiLightningPath = path.resolve(appNodeModules, '@svmx/ui-components-lightning');
-const uiLightningNodeModulesPath = path.resolve(uiLightningPath, 'node_modules');
 const uiPredixPath = path.resolve(appNodeModules, '@svmx/ui-components-predix');
 const uiLibBowerPath = path.resolve(uiPredixPath, 'bower_components');
 const uiLibBuiltBowerPath = path.resolve(uiPredixPath, 'build/polymer');
@@ -100,7 +99,7 @@ if (containsUILightningLibrary) {
   plugins.push(
     new CopyWebpackPlugin([
       {
-        context: path.resolve(uiLightningNodeModulesPath, '@salesforce-ux/design-system/assets'),
+        context: path.resolve(appNodeModules, '@salesforce-ux/design-system/assets'),
         from: '**/*',
         to: 'assets',
       },
